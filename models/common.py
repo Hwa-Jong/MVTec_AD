4dfe5aa66c206892adfc108efecb5b51708b9d43
Hwa-Jong <zergtos@a.ut.ac.kr>
2023-04-14 09:46:45 +0900

    modify model

--- models/common.py
+++ models/common.py
@@ -21,6 +21,20 @@ class ConvBlock(nn.Module):
     
 
 
+class Upsample(nn.Module):
+    def __init__(self, channels, scale, out_channels=None):
+        super(Upsample, self).__init__()
+        if out_channels is None:
+            out_channels = channels
+        self.conv = nn.Conv2d(in_channels=channels, out_channels=out_channels*scale*scale, kernel_size=1, stride=1, dilation=1, bias=True)
+        self.pixel_shuffle = nn.PixelShuffle(scale)
+        
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.pixel_shuffle(x)
+        return x
+
+
 class ConvResBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, dilation=1, bias=True, bn=True, act=nn.ReLU()):
         super(ConvResBlock, self).__init__()
--- models/models.py
+++ models/models.py
@@ -1,25 +1,42 @@
 import torch.nn as nn
 
-from .common import ConvResBlock, ConvBlock
+from .common import Upsample, ConvBlock
 
 class AE_student(nn.Module):
     def __init__(self):
         super(AE_student, self).__init__()
-        self.channel = [3, 32]
+        self.channel = [3, 8, 16, 32]
 
-        self.student = []
+        self.encoder = []
+        self.decoder = []
 
-        for i in range(len(self.channel)-1):
-            self.student.append( ConvBlock(in_channels=self.channel[i], out_channels=self.channel[i+1], kernel_size=3, bn=True, act=nn.ReLU()) )
+        # encoder
+        self.encoder.append( ConvBlock(in_channels=self.channel[0], out_channels=self.channel[1], kernel_size=3, bn=True, act=nn.ReLU()) )
+        self.encoder.append( ConvBlock(in_channels=self.channel[1], out_channels=self.channel[1], kernel_size=3, bn=True, act=nn.ReLU()) )
+        self.encoder.append( ConvBlock(in_channels=self.channel[1], out_channels=self.channel[1], kernel_size=3, bn=True, stride=2, act=nn.ReLU()) )
 
+        for i in range(1, len(self.channel)-1):
+            self.encoder.append( ConvBlock(in_channels=self.channel[i], out_channels=self.channel[i+1], kernel_size=3, bn=True, act=nn.ReLU()) )
+            self.encoder.append( ConvBlock(in_channels=self.channel[i+1], out_channels=self.channel[i+1], kernel_size=3, bn=True, stride=2, act=nn.ReLU()) )
+
+
+        # decoder
         for i in range(len(self.channel)-1, 1, -1):
-            self.student.append( ConvBlock(in_channels=self.channel[i], out_channels=self.channel[i-1], kernel_size=3, bn=True, act=nn.ReLU()) )
+            self.decoder.append( ConvBlock(in_channels=self.channel[i], out_channels=self.channel[i], kernel_size=3, bn=True, act=nn.ReLU()) )
+            self.decoder.append( Upsample(channels=self.channel[i], out_channels=self.channel[i-1], scale=2) )
 
-        self.student.append( nn.Conv2d(in_channels=self.channel[1], out_channels=self.channel[0], kernel_size=3, padding=1) )
+        self.decoder.append( ConvBlock(in_channels=self.channel[1], out_channels=self.channel[1], kernel_size=3, bn=True, act=nn.ReLU()) )
+        self.decoder.append( Upsample(channels=self.channel[1], out_channels=self.channel[1], scale=2) )
+
+        self.decoder.append( ConvBlock(in_channels=self.channel[1], out_channels=self.channel[1], kernel_size=3, bn=True, act=nn.ReLU()) )
+        self.decoder.append( ConvBlock(in_channels=self.channel[1], out_channels=self.channel[1], kernel_size=3, bn=True, act=nn.ReLU()) )
+        self.decoder.append( nn.Conv2d(in_channels=self.channel[1], out_channels=self.channel[0], kernel_size=3, padding=1) )
         
-        self.student = nn.Sequential(*self.student)
+        self.encoder = nn.Sequential(*self.encoder)
+        self.decoder = nn.Sequential(*self.decoder)
         
     def forward(self, x):  
-        x = self.student(x)
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
     

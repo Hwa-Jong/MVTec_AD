fc1f06eece805dc3d6102b9077cde781732357c9
Hwa-Jong <zergtos@a.ut.ac.kr>
2023-04-21 19:00:07 +0900

    add iou and roc

--- metric.py
+++ metric.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 
+from mvtec_ad_evaluation.roc_curve_util import compute_classification_roc
+from mvtec_ad_evaluation.generic_util import trapezoid
+
 def calculate_IoU(label, pred):
     '''
     label : label image (uint8 0~255)
@@ -14,3 +17,8 @@ def calculate_IoU(label, pred):
     iou = torch.sum(intersection, dim=[1,2,3]) / (torch.sum(union, dim=[1,2,3]) + 1e-6)
 
     return iou
+
+
+def calc_classification_roc(anomaly_maps, scoring_function, ground_truth_labels):
+    all_fprs, all_tprs = compute_classification_roc(anomaly_maps, scoring_function, ground_truth_labels)
+    return trapezoid(all_fprs, all_tprs)
